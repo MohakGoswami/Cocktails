6d9c276abe9e5d8a691314725048b55209d1ea4d
Mohak Goswami <mohak.g.ji@gmail.com>
2022-11-25 00:32:11 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia saepe
+        laboriosam odio nihil quod sint ipsa optio laudantium quis id dolor,
+        accusantium voluptas, exercitationem ullam aut quibusdam possimus libero
+        vel in incidunt non iste, magnam maxime. Impedit rem et animi?
+      </p>
+    </section>
+  );
+};
+
+export default About;
